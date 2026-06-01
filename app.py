c481c43367d1effaa5b0daf19df8b90dfc5c347f
wyangsunny727 <mandy.wan727@gmail.com>
2026-06-01 14:24:18 +1000

    Update app.py

--- app.py
+++ app.py
@@ -126,8 +126,7 @@ with st.spinner("Analyzing live market trends safely via data cache..."):
         # Signal Matrix
         if latest_rsi < 35 and pred_change_pct > 0  and pe_ratio < 80:
             signal = "🟢 Strong Buy"
-        elif (latest_rsi < 45 and pe_ratio < 80) or (
-            pred_change_pct > 5):
+        elif (latest_rsi < 45 and pe_ratio < 80) or pred_change_pct > 5:
             signal = "🟡 Accumulate"
         elif latest_rsi > 70:
             signal = "🔴 Overbought"
